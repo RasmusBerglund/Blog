f9e3db9c725832102aa3058a955ed75509f973ae
Rasmus Berglund <rasmus.berglund@zenta.se>
2018-02-02 15:44:13 +0100

    redux and route is now implemented

--- reactblog/src/App.js
+++ reactblog/src/App.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
+import { Route, Link } from 'react-router-dom'
 import logo from './logo.svg';
 import './App.css';
 import sanity from './lib/sanity';
-import {BlogList} from './componets/blogList'
+import { BlogList } from './componets/blogList'
+import Home from './componets/Home/home'
+import About from './componets/About/about'
 
 var Loader = require('react-loader');
 
@@ -36,11 +39,15 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to my blog</h1>
+          <Link to="/">Home</Link>
+          <Link to="/about-us">About</Link>
         </header>
         <div className="container">
-        <Loader loaded={this.state.loaded} className="App-intro">
-          <BlogList posts={this.state.posts} />
-        </Loader>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/about-us" component={About} />
+          <Loader loaded={this.state.loaded} className="App-intro">
+            <BlogList posts={this.state.posts} />
+          </Loader>
         </div>
       </div>
     );
--- reactblog/src/index.js
+++ reactblog/src/index.js
@@ -2,8 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux'
+import { ConnectedRouter } from 'react-router-redux'
+import store, { history } from './store-index'
 import registerServiceWorker from './registerServiceWorker';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const target = document.querySelector('#root')
+
+ReactDOM.render(
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <div>
+        <App />
+      </div>
+    </ConnectedRouter>
+  </Provider>,
+  target
+)
