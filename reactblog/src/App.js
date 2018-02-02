45674c8ec46037c79ea496000c8e6f68606403e9
Rasmus Berglund <rasmus.berglund@zenta.se>
2018-02-02 16:01:08 +0100

    better routing

--- reactblog/src/App.js
+++ reactblog/src/App.js
@@ -2,37 +2,10 @@ import React, { Component } from 'react';
 import { Route, Link } from 'react-router-dom'
 import logo from './logo.svg';
 import './App.css';
-import sanity from './lib/sanity';
-import { BlogList } from './componets/blogList'
 import Home from './componets/Home/home'
 import About from './componets/About/about'
 
-var Loader = require('react-loader');
-
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      posts: [],
-      loaded: false,
-    };
-  }
-
-  async componentDidMount() {
-    await sanity
-      .fetch(
-      '*[_type == $type][0...5]',
-      { type: 'post' }
-      )
-      .then(res => {
-        this.setState({ posts: res, loaded: true })
-        console.log(res)
-      })
-      .catch(err => {
-        console.error('Oh no, error occured: ', err)
-      })
-  }
-
   render() {
     return (
       <div className="App">
@@ -45,9 +18,6 @@ class App extends Component {
         <div className="container">
           <Route exact path="/" component={Home} />
           <Route exact path="/about-us" component={About} />
-          <Loader loaded={this.state.loaded} className="App-intro">
-            <BlogList posts={this.state.posts} />
-          </Loader>
         </div>
       </div>
     );
--- reactblog/src/componets/Home/home.js
+++ reactblog/src/componets/Home/home.js
@@ -1,21 +1,53 @@
-import React from 'react'
+import React, {Component} from 'react'
+import { Route, Link } from 'react-router-dom'
 import { push } from 'react-router-redux'
-import { bindActionCreators } from 'redux' 
+import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
+import sanity from '../../lib/sanity';
+import { BlogList } from '../blogList'
 
-const Home = props => (
-  <div>
-    <h1>Home</h1>
-    <p>Welcome home!</p>
-    <button onClick={() => props.changePage()}>Go to about page via redux</button>
-  </div>
-)
+var Loader = require('react-loader');
+
+class Home extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          posts: [],
+          loaded: false,
+        };
+      }
+    
+      async componentDidMount() {
+        await sanity
+          .fetch(
+          '*[_type == $type][0...5]',
+          { type: 'post' }
+          )
+          .then(res => {
+            this.setState({ posts: res, loaded: true })
+            console.log(res)
+          })
+          .catch(err => {
+            console.error('Oh no, error occured: ', err)
+          })
+      }
+    
+      render() {
+        return (
+            <div className="container">
+              <Loader loaded={this.state.loaded} className="App-intro">
+                <BlogList posts={this.state.posts} />
+              </Loader>
+            </div>
+        );
+      }
+    }
 
 const mapDispatchToProps = dispatch => bindActionCreators({
-  changePage: () => push('/about-us')
+    changePage: () => push('/about-us')
 }, dispatch)
 
 export default connect(
-  null, 
-  mapDispatchToProps
+    null,
+    mapDispatchToProps
 )(Home)
